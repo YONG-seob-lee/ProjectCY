a759de60dd72ba81121deba08b67d6ecb5513b08
YONG-seob-lee <dydtjql12@naver.com>
2024-03-08 21:16:47 +0900

    [이용섭] Add - [시스템] ProjectCY 첫 커밋

---
+++ Source/ProjectCY/ProjectCYGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectCYGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCY_API AProjectCYGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
